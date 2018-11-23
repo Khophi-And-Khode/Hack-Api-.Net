0332cefe889cbd04f686c37365af41b3ab0ca274
SAMUELWENDOLIN KETECHIE <samuelwendolin2@live.com>
2018-11-23 09:51:00 +0000

    mysql db operations

---
+++ Hack-Api-.Net/Model/ImageData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackApi.Model
+{
+    public class ImageData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public string Signature { get; set; }
+        public string PublicId { get; set; }
+        public string Url { get; set; }
+        public string SecureUrl { get; set; }
+        public string Format { get; set; }
+        public string ResourceType { get; set; }
+
+    }
+}
