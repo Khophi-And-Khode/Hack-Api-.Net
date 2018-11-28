7d70cfc77d878b54f0ec4dc78812e75654003454
SAMUELWENDOLIN KETECHIE <samuelwendolin2@live.com>
2018-11-28 07:06:58 +0000

    added saving into products table

---
+++ Hack-Api-.Net/Model/ProductImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackApi.Model
+{
+    public class ProductImage
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Content { get; set; }
+        public string Implication { get; set; }
+        public string Reviews { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
